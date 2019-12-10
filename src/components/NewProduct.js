a9d2b94523eb75ae2962294977f94e9f7423d82f
Jaime Duque <jaime@Jaimes-MacBook-Pro.local>
2019-12-09 22:53:29 -0500

    The products reducer is created

--- src/components/NewProduct.js
+++ src/components/NewProduct.js
@@ -1,19 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const NewProduct = () => {
+
+    // state
+    const [name, saveName] = useState('');
+    const [price, savePrice] = useState('');
+
+    // add new product
+    const submitNewProduct = e => {
+        e.preventDefault();
+
+        // form validate
+        if (name.trim() === '' || price.trim() === '') {
+            console.log('error validation');
+            return;
+        }
+
+        // if pass the validation
+
+
+        // create new product
+
+
+        // redirect
+    } 
+
     return (
         <div className="row justify-content-center mt-5">
             <div className="col-md-8">
                 <div className="card">
                     <div className="card-body">
                         <h2 className="text-center mb-4 font-weight-bold ">Add New Book</h2>
-                        <form>
+                        <form onSubmit={submitNewProduct}>
                             <div className="form-group">
                                 <label>Book Name</label>
                                 <input
                                     type="text"
                                     className="form-control"
                                     placeholder="Book Name"
+                                    value={name}
+                                    onChange={e => saveName(e.target.value)}
                                 />
                             </div>
                             <div className="form-group">
@@ -22,6 +48,8 @@ const NewProduct = () => {
                                     type="text"
                                     className="form-control"
                                     placeholder="Book Price"
+                                    value={price}
+                                    onChange={e => savePrice(e.target.value)}
                                 />
                             </div>
 
